edf8e2cb305dacf47162df4a8d145022d7be2a77
DaliborA <d.arula@adriamedia.hr>
2021-11-12 13:18:13 +0100

    newsletter checkbox
    Added a new checkbox, new validation and new destination spreadsheet.

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,16 +1,22 @@
 // Anketa
 document.querySelector('#submit-form').addEventListener('click', function(e){
 
-  const url = 'https://script.google.com/macros/s/AKfycbwT0lI-n24QHIHzWp8Bxj7ZyseWheOx0r8xDKW9RMC8N11zj8MJ3hvuHuOeqdeGGCjaLQ/exec';
+  // old sheet
+  // const url = 'https://script.google.com/macros/s/AKfycbwT0lI-n24QHIHzWp8Bxj7ZyseWheOx0r8xDKW9RMC8N11zj8MJ3hvuHuOeqdeGGCjaLQ/exec';
+
+  // newsletter
+  const url = 'https://script.google.com/macros/s/AKfycbyXU69gHVOY7yYXzOp7s_RnDRd7pzFm8RntJnDq8zGsrbw-6sM1g_r5-UnpqXWO7idx/exec';
   
 
   e.preventDefault();
 
-  if(document.getElementById('pravila').checked) {
+  if(document.getElementById('pravila').checked && document.getElementById('newsletter').checked) {
 
       document.querySelector('#submit-form').classList.add('is-hidden');
       document.querySelector('#error-msg').classList.add('is-hidden');
       document.querySelector('#loading-indicator').classList.remove('is-hidden');
+
+      console.log($('form#anketaArena').serializeJSON());
 
       fetch(url,{
         method: 'POST',
